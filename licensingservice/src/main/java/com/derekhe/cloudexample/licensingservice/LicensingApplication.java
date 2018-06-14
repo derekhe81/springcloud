41720821bb5074df8aebe71bcfb7e97fdf8a6a9e
hexuan <he.xuan@live.cn>
2018-06-14 23:34:39 +1000

    fix broken build

--- licensingservice/src/main/java/com/derekhe/cloudexample/licensingservice/LicensingApplication.java
+++ licensingservice/src/main/java/com/derekhe/cloudexample/licensingservice/LicensingApplication.java
@@ -6,6 +6,8 @@ package com.derekhe.cloudexample.licensingservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.ApplicationContext;
 
 /**
@@ -13,6 +15,8 @@ import org.springframework.context.ApplicationContext;
  *
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 @RefreshScope
 public class LicensingApplication {
 
@@ -22,10 +26,10 @@ public class LicensingApplication {
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(LicensingApplication.class, args);
 		
-//            for (String name: applicationContext.getBeanDefinitionNames()) {
-//                System.out.println("------------------------");
-//            	System.out.println(name);
-//            }		
+            for (String name: applicationContext.getBeanDefinitionNames()) {
+                System.out.println("------------------------");
+            	System.out.println(name);
+            }		
 
 	}
 
--- licensingservice/src/main/java/com/derekhe/cloudexample/licensingservice/clients/OrganizationDiscoveryClient.java
+++ licensingservice/src/main/java/com/derekhe/cloudexample/licensingservice/clients/OrganizationDiscoveryClient.java
@@ -4,8 +4,10 @@ package com.derekhe.cloudexample.licensingservice.clients;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -18,9 +20,12 @@ public class OrganizationDiscoveryClient {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+    
+    @Autowired
+    RestTemplate restTemplate;
 
     public Organization getOrganization(String organizationId) {
-        RestTemplate restTemplate = new RestTemplate();
+//        RestTemplate restTemplate = new RestTemplate();
         List<ServiceInstance> instances = discoveryClient.getInstances("organizationservice");
 
         if (instances.size()==0) return null;
@@ -34,4 +39,9 @@ public class OrganizationDiscoveryClient {
 
         return restExchange.getBody();
     }
+    
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}    
 }
